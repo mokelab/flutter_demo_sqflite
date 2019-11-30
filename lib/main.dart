d8e78ded21331edb11cdadf58f0e884ab6cd4ad4
Hirokazu Fukami <fkm@mokelab.com>
2019-11-30 16:18:01 +0900

    Implement create get delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,38 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+
+Database db;
+Random rand = new Random();
+
+void main() async {
+  db = await openDatabase("my_db.db", version: 1,
+      onCreate: (database, version) async {
+    await database.execute('''create table account(
+          _id integer primary key autoincrement,
+          name text,
+          age int
+          )''');
+  });
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'sqflite demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'sqflite demo app'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +41,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Account> _accounts = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void didChangeDependencies() {
+    _refreshList();
+    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      body: ListView.builder(
+        itemCount: _accounts.length,
+        itemBuilder: (con, index) {
+          var account = _accounts[index];
+          return ListTile(
+            title: Text(account.name),
+            subtitle: Text("Age ${account.age}"),
+            onTap: () {
+              _removeAccount(account);
+            },
+          );
+        },
+      ).build(context),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _addAccount,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
+
+  void _addAccount() async {
+    await _insertAccount(db, _makeName(), _makeAge());
+    _refreshList();
+  }
+
+  void _refreshList() async {
+    List<Account> accounts = await _getAllAccount(db);
+    setState(() {
+      _accounts.clear();
+      _accounts.addAll(accounts);
+    });
+  }
+
+  void _removeAccount(Account account) async {
+    await _deleteAccount(db, account);
+    _refreshList();
+  }
+}
+
+List<String> _names = ["moke", "piyo", "mallo", "bill", "steve", "zack"];
+
+String _makeName() {
+  return _names[rand.nextInt(1000) % 6];
+}
+
+int _makeAge() {
+  return rand.nextInt(30) + 5;
+}
+
+class Account {
+  final int id;
+  final String name;
+  final int age;
+
+  Account(this.id, this.name, this.age);
+}
+
+Future _insertAccount(Database db, String name, int age) async {
+  var values = <String, dynamic>{
+    "name": name,
+    "age": age,
+  };
+  await db.insert("account", values);
+}
+
+Future<List<Account>> _getAllAccount(Database db) async {
+  List<Map> results = await db.query("account");
+  // map to account list
+  return results.map((Map m) {
+    int id = m["_id"];
+    String name = m["name"];
+    int age = m["age"];
+    return Account(id, name, age);
+  }).toList();
+}
+
+Future _deleteAccount(Database db, Account account) async {
+  await db.delete("account", where: "_id=?", whereArgs: [account.id]);
 }
